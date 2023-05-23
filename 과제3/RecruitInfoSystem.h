67221a6b229eb364bba4e2d55b71bd33fa71381d
shnamm <96981162+shnamm@users.noreply.github.com>
2023-05-23 22:43:18 +0900

    added

--- 과제3/RecruitInfoSystem.h
+++ 과제3/RecruitInfoSystem.h
@@ -1,1 +1,48 @@
 
+/*
+채용정보
+RecruitInfo : Entity Class
+작성자 : 남석현
+*/
+
+/*
+회사
+Company : Entity Class
+작성자 : 남석현
+*/
+
+/*
+채용정보등록
+AddRecruitInfoUI : Boundary Class
+작성자 : 남석현
+*/
+
+/*
+채용정보등록
+AddRecruitInfo : Control Class
+작성자 : 남석현
+*/
+
+/*
+채용정보조회
+CheckRecruitInfoUI : Boundary Class
+작성자 : 남석현
+*/
+
+/*
+채용정보조회
+CheckRecruitInfo : Control Class
+작성자 : 남석현
+*/
+
+/*
+지원정보통계
+RecruitInfoStaticUI : Boundary Class
+작성자 : 남석현
+*/
+
+/*
+지원정보통계
+RecruitInfoStatic : Control Class
+작성자 : 남석현
+*/
