94b01ac41bc5f84acd136ba987e4177315f74697
waterhyacinth <97887725+cjsgmount@users.noreply.github.com>
2023-05-23 21:21:10 +0900

    Update RecruitInfoSystem.cpp

--- 과제3/RecruitInfoSystem.cpp
+++ 과제3/RecruitInfoSystem.cpp
@@ -1,1 +1,112 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <vector>
 
+#include "Class.h"
+
+#define MAX_STRING 32
+#define INPUT_FILE_NAME "input.txt"
+#define OUTPUT_FILE_NAME "output.txt"
+
+using namespace std;
+
+
+void doTask()
+{	// 메뉴 파싱을 위한 level 구분을 위한 변수
+	int menu_level_1 = 0, menu_level_2 = 0; int is_program_exit = 0;
+		
+	while (!is_program_exit) { 
+		// 입력파일에서 메뉴 숫자 2개를 읽기
+		inputFile >> menu_level_1 >> menu_level_2;
+
+		// 메뉴 구분 및 해당 연산 수행
+		switch (menu_level_1) {
+			case 1: { switch (menu_level_2) {
+				case 1: // "1.1. 회원가입" 메뉴 부분
+				{	// join() 함수에서 해당 기능 수행 
+					join();
+					break; 
+				} 
+				case 2:	// "1.2. 회원탈퇴" 메뉴 부분
+				{	// withdrawal() 함수에서 해당 기능 수행
+					withdrawal();
+					break; 
+				}
+
+			}}
+
+			case 2: { switch (menu_level_2) {
+				case 1: // "2.1. 로그인" 메뉴 부분
+				{	// login() 함수에서 해당 기능 수행
+					login();
+					break;
+				}
+				case 2: // "2.2. 로그아웃" 메뉴 부분
+				{	// logout() 함수에서 해당 기능 수행
+					logout();
+					break;
+				}
+
+			}}
+
+			case 3: { switch (menu_level_2) {
+				case 1: // "3.1. 채용 정보 등록" 메뉴 부분
+				{	// () 함수에서 해당 기능 수행 
+					();
+					break;
+				}
+				case 2:	// "3.2. 등록된 채용 정보 조회" 메뉴 부분
+				{	// () 함수에서 해당 기능 수행
+					();
+					break;
+				}
+
+			}}
+			
+			case 4: { switch (menu_level_2) {
+				case 1: // "4.1. 채용 정보 검색" 메뉴 부분
+				{	// () 함수에서 해당 기능 수행 
+					();
+					break;
+				}
+				case 2:	// "4.2. 채용 지원" 메뉴 부분
+				{	// () 함수에서 해당 기능 수행
+					();
+					break;
+				}
+				case 3:	// "4.3. 지원 정보 조회" 메뉴 부분
+				{	// () 함수에서 해당 기능 수행
+					();
+					break;
+				}
+				case 4:	// "4.4. 지원 취소" 메뉴 부분
+				{	// () 함수에서 해당 기능 수행
+					();
+					break;
+				}
+
+			}}
+
+			case 5: { switch (menu_level_2) {
+				case 1: // "5.1. 지원 정보 통계" 메뉴 부분
+				{	// () 함수에서 해당 기능 수행 
+					();
+					break;
+				}
+
+			}}
+
+			case 6: { switch (menu_level_2) {
+				case 1: // "6.1. 종료“ 메뉴 부분
+				{ 
+					program_exit(); 
+					is_program_exit = 1; 
+					break;
+				}
+			}}
+				
+				  return;
+		}
+	}
+}
