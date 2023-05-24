8f6ba44b80452be7c13d3e608d0653392f089865
Sanghyuk Kim <81136816+qimsh7@users.noreply.github.com>
2023-05-24 13:34:57 +0900

    Update RecruitInfoSystem.h
    1. 본인 소스코드 붙여넣기

--- 과제3/RecruitInfoSystem.h
+++ 과제3/RecruitInfoSystem.h
@@ -1,17 +1,18 @@
+#ifndef RecruitInfoSystem_h
+#define RecruitInfoSystem_h
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <vector>
-
-#define CLASS_H
 
 #define INPUT_FILE_NAME "input.txt"
 #define OUTPUT_FILE_NAME "output.txt"
 
 using namespace std;
 
-ifstream inputFile;
-ofstream outputFile;
+ifstream inputFile(INPUT_FILE_NAME);   // input.txt 를 읽기모드로 열기
+ofstream outputFile(OUTPUT_FILE_NAME); // output.txt 를 쓰기모드로 열기
 
 
 void doTask();
@@ -166,3 +167,134 @@ RecruitInfoStaticUI : Boundary Class
 RecruitInfoStatic : Control Class
 작성자 : 남석현
 */
+
+
+
+
+/*
+ ApplyInfo Entity Classes
+ 작성자: 김상혁
+ */
+class ApplyInfo{
+private:
+    string companyName;  // 회사이름
+    string work;  // 업무
+    int numPeople;  // 인원수
+    string deadline;  // 신청 마감일
+    string entrepreneurNumber;  // 사업자 번호
+public:
+    void getApplyInfoDetail();
+    
+    // get 함수
+    string getCompanyName();
+    string getWork();
+    int getNumPeople();
+    string getDeadline();
+    string getEntrepreneurNumber();
+};
+
+
+
+
+/*
+ Applicant Entity Classes
+ 작성자: 김상혁
+ */
+class Applicant{
+private:
+    vector <ApplyInfo*> applyInfoList;  // 지원목록
+public:
+    void getApplyInfo();
+    void deleteApplyInfo(string entrepreneurNumber);
+    void getApplyNumsPerWork();
+    
+    // get 함수
+    vector <ApplyInfo*> getApplyInfoList();
+};
+
+
+
+
+/*
+ 지원정보조회 Control Class
+ 작성자: 김상혁
+ */
+class CheckApplyInfo{
+private:
+    
+public:
+    CheckApplyInfo();
+    void showApplyInfo();
+};
+
+
+/*
+ 지원정보조회 Boundary Class
+ 작성자: 김상혁
+ */
+class CheckApplyInfoUI{
+private:
+    
+public:
+    void checkApplyInfoButton(CheckApplyInfo* checkApplyInfo);
+};
+
+
+
+
+/*
+ 지원취소 Control Class
+ 작성자: 김상혁
+ */
+class CancelApply{
+private:
+    
+public:
+    CancelApply();
+    void sendCancelApplyData(string entrepreneurNumber);
+};
+
+
+/*
+ 지원취소 Boundary Class
+ 작성자: 김상혁
+ */
+class CancelApplyUI{
+private:
+    
+public:
+    void inputCancelApplyData(CancelApply* cancelApply);
+};
+
+
+
+
+/*
+ 지원정보통계 Control Class
+ 작성자: 김상혁
+ */
+class ApplyInfoStatistic{
+private:
+    
+public:
+    ApplyInfoStatistic();
+    void showStatistic();
+};
+
+
+
+/*
+ 지원정보통계 Boundary Class
+ 작성자: 김상혁
+ */
+class ApplyInfoStatisticUI{
+private:
+    
+public:
+    void applyInfoStatisticButton(ApplyInfoStatistic* applyInfoStatistic);
+};
+
+
+
+
+#endif /* RecruitInfoSystem_h */
