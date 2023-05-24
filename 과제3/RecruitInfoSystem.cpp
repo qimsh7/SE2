80a82629160784643b07a57a8bd9f0d2478010b1
waterhyacinth <97887725+cjsgmount@users.noreply.github.com>
2023-05-24 13:23:03 +0900

    Update RecruitInfoSystem.cpp

--- 과제3/RecruitInfoSystem.cpp
+++ 과제3/RecruitInfoSystem.cpp
@@ -118,6 +118,149 @@ void doTask()
 	}
 }
 
+//기능: 회원가입
+void AddMemberUI::joinNewMember(vector<Member>& members)
+{	
+	AddMember addMember;
+	addMember.createNewMember(members);
+}
+
+void AddMember::createNewMember(vector<Member>& members)
+{
+	Member member;
+	member.addNewMember(members);
+}
+
+void Member::addNewMember(vector<Member>& members)
+{
+	string name, number, ID, PW;
+
+	int memberCategory = 0;				//일반회원인지 회사회원인지 구분
+
+	// 입력 형식 : 이름, 주민번호, ID, Password를 파일로부터 읽음
+	inputFile >> memberCategory;
+
+	if (memberCategory == 1)			// 회사회원 회원가입
+	{
+		inputFile >> name >> number >> ID >> PW;
+
+		CompanyMember companyMember(name, number);	// 생성자를 통해 companyName과 entrepreneurNumber 설정
+		companyMember.ID = ID;
+		companyMember.PW = PW;
+		companyMember.loggedIn = 0; // 로그아웃 상태로 초기화
+
+		members.push_back(companyMember);
+
+	}
+
+	else if (memberCategory == 2)		// 일반회원 회원가입
+	{
+		inputFile >> name >> number >> ID >> PW; 
+
+		NormalMember normalMember(name, number);	// 생성자를 통해 name과 residentNumber 설정
+		normalMember.ID = ID;
+		normalMember.PW = PW;
+		normalMember.loggedIn = 0; // 로그아웃 상태로 초기화
+
+		members.push_back(normalMember);
+
+	}
+
+	outputFile << "1.1. 회원가입" << endl;
+	outputFile << "> " << memberCategory << " " << name << " " << number << " " << ID << " " << PW << endl;
+
+}
+
+
+//기능 : 회원탈퇴
+void WithdrawalUI::requestWithdrawal(vector<Member>& members)
+{
+	Withdrawal withdrawal;
+	withdrawal.withdrawal(members);
+}
+
+void Withdrawal::withdrawal(vector<Member>& members)
+{
+	Member member;
+	member.deleteMember(members);
+}
+
+void Member::deleteMember(vector<Member>& members)
+{
+	string targetID;
+	inputFile >> targetID;
+
+	for (auto it = members.begin(); it != members.end(); ++it)
+	{
+		if (it->getID() == targetID)
+		{
+			members.erase(it);
+			break;
+		}
+	}
+
+	outputFile << "1.2. 회원탈퇴" << endl;
+	outputFile << "> " << targetID << endl;
+
+}
+
+
+//기능 : 로그인
+void LoginUI::login(vector<Member>& members)
+{
+	Login login;
+	login.getLoginInformation(members);
+}
+
+void Login::getLoginInformation(vector<Member>& members)
+{
+	Member member;
+	member.checkMember(members);
+}
+
+void Member::checkMember(vector<Member>& members)
+{
+	string targetID;
+	string targetPW;
+
+	inputFile >> targetID >> targetPW;
+
+	for (Member& member : members) {
+		if (member.getID() == targetID && member.getPW() == targetPW) {
+			// 로그인 성공
+			member.setLoggedIn(1);
+
+			outputFile << "2.1. 로그인" << endl;
+			outputFile << "> " << targetID << " " << targetPW << endl;
+
+			return;
+		}
+	}
+	
+}
+
+
+//기능 : 로그아웃
+void LogoutUI::requestLogout(vector<Member>& members)
+{
+	Logout logout;
+	logout.logout(members);
+}
+
+void Logout::logout(vector<Member>& members)
+{
+	string targetID;
+
+	for (Member& member : members) {
+		if (member.getID() == targetID) {
+			// 로그아웃 처리
+			member.setLoggedIn(0);
+
+			outputFile << "2.2. 로그아웃" << endl;
+			outputFile << "> " << targetID << endl;
+		}
+	}
+}
 
 
 
