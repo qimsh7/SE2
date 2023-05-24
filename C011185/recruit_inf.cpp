0608717c28d41bed5124b4f7114400b4a8bcfd64
Imjyunhyeok <103239637+Imjyunhyeok@users.noreply.github.com>
2023-05-24 20:43:20 +0900

    Update recruit_inf.cpp

--- C011185/recruit_inf.cpp
+++ C011185/recruit_inf.cpp
@@ -1,32 +1,145 @@
-#include "recruit_info.h"
+#include "RecruitInfoSystem.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
-RecruitInfo::RecruitInfo(const std::string& name, int number, const std::string& job, int people, const std::string& dueDate)
-    : companyName(name), entrepreneurNumber(number), work(job), numPeople(people), deadline(dueDate) {}
+#define MAX_STRING 32
+#define INPUT_FILE_NAME "input.txt"
+#define OUTPUT_FILE_NAME "output.txt"
 
-void RecruitInfo::searchByCompanyName(const std::vector<RecruitInfo>& recruitInfos, const std::string& searchName) {
-    for (const auto& recruitInfo : recruitInfos) {
-        if (recruitInfo.companyName == searchName) {
-            std::cout << recruitInfo.companyName << " "
-                << recruitInfo.entrepreneurNumber << " "
-                << recruitInfo.work << " "
-                << recruitInfo.numPeople << " "
-                << recruitInfo.deadline << std::endl;
-            return;
-        }
-    }
+using namespace std;
+ 
 
-    std::cout << "해당 회사 정보를 찾을 수 없습니다." << std::endl;
+/*
+ ApplyUI::selectentrepreneur(Apply* apply)
+ 사용되는 곳: 채용 지원
+ 작성자: 임준혁
+ */
+void ApplyUI::selectentrepreneur(Apply* apply)
+{
+    apply->showapplyrcruit();
 }
 
-void RecruitInfo::searchByEntrepreneurNumber(const std::vector<RecruitInfo>& recruitInfos, int searchNumber) {
-    for (const auto& recruitInfo : recruitInfos) {
-        if (recruitInfo.entrepreneurNumber == searchNumber) {
-            std::cout << recruitInfo.companyName << " "
-                << recruitInfo.entrepreneurNumber << " "
-                << recruitInfo.work << std::endl;
-            return;
-        }
-    }
 
-    std::cout << "해당 회사 정보를 찾을 수 없습니다." << std::endl;
+/*
+ Apply::Apply()  // 생성자
+ 사용되는 곳: 채용지원
+ 작성자: 임준혁
+ */
+Apply::Apply()
+{
+        ApplyUI* applyUI = new ApplyUI;
+        applyUI->selectentrepreneur(this); // this는 무엇일까? cpp에서 받아오나보다
 }
+
+/*
+ Apply::showapplyrecruit()
+ 사용되는 곳: 채용지원
+ 작성자: 임준혁
+ */
+void Apply::showapplyrecruit()
+{
+    (뭐라고 써야하나)->listRecruitInfo();
+}
+
+
+/*
+ ResearchRecruitInfoUI::selectCompany(ResearchRecruitInfo* researchrecruitinfo)
+ 사용되는 곳: 채용 정보 검색
+ 작성자: 임준혁
+ */
+void ResearchRecruitInfoUI::selectCompany(ResearchRecruitInfo* researchrecruitinfo)
+{
+    researchrecruitinfo->showRecruitInfo();
+}
+
+/*
+ ResearchRecruitInfo::ResearchRecruitInfo()  // 생성자
+ 사용되는 곳: 채용 정보 검색
+ 작성자: 임준혁
+ */
+ResearchRecruitInfo::ResearchRecruitInfo()
+{
+        ResearchRecruitInfoUI* researchrecruitInfoUI = new ResearchRecruitInfoUI;
+        researchrecruitInfoUI->selectCompany(this); //새로 만든 저 인스턴스(?)라는 애에서 selectCompany갖고 뭐하나봐..
+}
+
+/*
+ Apply::showapplyrecruit()
+ 사용되는 곳: 채용지원
+ 작성자: 임준혁
+ */
+void Apply::showRecruitInfo()
+{
+    (뭐라고 써야하나)->getRecruitInfo();
+}
+
+
+//공통 Entity부분
+<Entity : RecruitInfo 부분>
+/*
+ RecruitInfo::getRecruitInfo()
+ 사용되는 곳: 채용 정보 검색, 채용 지원
+ 작성자: 임준혁
+ */
+void RecruitInfo::getRecruitInfoDetail() // 정보들 5개 받아와서 출력하기
+{
+    // 출력 양식
+    outputFile << getCompanyName() << " " << getEntrepreneurNumber() << “ ” < getWork() << " " << getNumPeople() << " " << getDeadline() << endl;
+}
+
+
+/*
+ RecruitInfo::getCompanyName()
+ 사용되는 곳: 채용 정보 검색, 채용 지원
+ 작성자: 임준혁
+ */
+string RecruitInfo::getCompanyName() // 회사이름 받아오기
+{
+    return this->companyName;
+}
+
+
+/*
+ RecruitInfo::getWork()
+ 사용되는 곳: 채용 정보 검색, 채용 지원
+ 작성자: 임준혁
+ */
+string RecruitInfo::getWork() // 업무 받아오기
+{
+    return this->work;
+}
+
+
+/*
+ RecruitInfo::getNumPeople()
+ 사용되는 곳: 채용 정보 검색, 채용 지원
+ 작성자: 임준혁
+ */
+int RecruitInfo::getNumPeople() // 인원수 받아오기
+{
+    return this->numPeople;
+}
+
+
+/*
+ RecruitInfo::getDeadline()
+ 사용되는 곳: 채용 지원 검색, 채용 지원
+ 작성자: 임준혁
+ */
+string RecruitInfo::getDeadline() // 마감일 받아오기
+{
+    return this->deadline;
+}
+
+
+/*
+ RecruitInfo::getEntrepreneurNumber()
+ 사용되는 곳: 채용 정보 검색, 채용 지원
+ 작성자: 
+ */
+string RecruitInfo::getEntrepreneurNumber() // 사업자번호 받아오기
+{
+    return this->entrepreneurNumber;
